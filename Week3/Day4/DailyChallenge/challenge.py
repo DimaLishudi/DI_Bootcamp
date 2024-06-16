d38bd7244bff4f219ba78450d5adc29038aacb13
Dmitry Lishudi <lishudi.dmitry@gmail.com>
2024-06-16 12:30:37 +0300

    solve w3/d4 challenge

--- Week3/Day4/DailyChallenge/challenge.py
+++ Week3/Day4/DailyChallenge/challenge.py
@@ -1,84 +1,62 @@
-# Instructions : Old MacDonald’s Farm
-# Take a look at the following code and output:
-# File: market.py
-
-# macdonald = Farm("McDonald")
-# macdonald.add_animal('cow',5)
-# macdonald.add_animal('sheep')
-# macdonald.add_animal('sheep')
-# macdonald.add_animal('goat', 12)
-# print(macdonald.get_info())
-# Output
-
-# McDonald's farm
-
-# cow : 5
-# sheep : 2
-# goat : 12
-
-#     E-I-E-I-0!
+import pathlib
+# Instructions :
+# The goal of the exercise is to create a class that will help you analyze a specific text. A text can be just a simple string, like “Today, is a happy day” or it can be an external text file.
 
 
-# Create the code that is needed to receive the result provided above. Below are a few questions to assist you with your code:
+# Part I
+# First, we will analyze a simple string, like “A good book would sometimes cost as much as a good house.”
 
-# Create a class called Farm. How should it be implemented?
-# Does the Farm class need an __init__ method? If so, what parameters should it take?
-# How many methods does the Farm class need?
-# Do you notice anything interesting about the way we are calling the add_animal method? What parameters should this function have? How many…?
-# Test your code and make sure you get the same results as the example above.
-# Bonus: nicely line the text in columns as seen in the example above. Use string formatting.
+# Create a class called Text that takes a string as an argument and store the text in a attribute.
+# Hint: You need to manually copy-paste the text, straight into the code
 
+# Implement the following methods:
+# a method to return the frequency of a word in the text (assume words are separated by whitespace) return None or a meaningful message.
+# a method that returns the most common word in the text.
+# a method that returns a list of all the unique words in the text.
 
-# Expand The Farm
-# Add a method called get_animal_types to the Farm class. This method should return a sorted list of all the animal types (names) in the farm. With the example above, the list should be: ['cow', 'goat', 'sheep'].
+# Part II
+# Then, we will analyze a text coming from an external text file. Download the_stranger.txt file.
 
-# Add another method to the Farm class called get_short_info. This method should return the following string: “McDonald’s farm has cows, goats and sheeps.”. The method should call the get_animal_types function to get a list of the animals.
-# Note : In English the plural form of the word “sheep” is sheep. But for the purpose of the exercise, let’s say that if there is more than 1 animal, an “s” should be added at the end of the word.
+# Implement a classmethod that returns a Text instance but with a text file:
 
-
-class Farm:
-    def __init__(self, name: str) -> None:
-        self.name = name
-        self.animals: dict[str, int] = {}
-        self.tab: int = 0
-
-    def add_animal(self, animal_name: str, amount: int = 1) -> None:
-        if animal_name not in self.animals:
-            self.animals[animal_name] = amount
-        else:
-            self.animals[animal_name] += amount
-        self.tab = max(self.tab, len(animal_name))
-
-    def get_info(self) -> None:
-        print(f"{self.name}'s farm\n")
-        for animal_name, amount in self.animals.items():
-            print(f"{animal_name:<{self.tab}} : {amount}")
-        print("\n")
-        print("    E-I-E-I-0!")
-
-    def get_animal_types(self) -> list[str]:
-        return list(sorted(self.animals.keys()))
-
-    def get_short_info(self) -> None:
-        animals: list[str] = self.get_animal_types()
-        if len(animals) == 0:
-            print(f"{self.name}'s farm doesn't have any animals")
-            return
-        if len(animals) == 1:
-            print(f"{self.name}'s farm only has {animals[0]}")
-            return
-        print_str = f"{self.name}'s farm has "
-        for animal in animals[:-2]:
-            print_str += animal + "s, "
-        print_str += animals[-2] + "s and " + animals[-1] + "s."
-        print(print_str)
+#     >>> Text.from_file('the_stranger.txt')
+# Hint: You need to open and read the text from the text file.
 
 
-macdonald = Farm("McDonald")
-macdonald.add_animal('cow', 5)
-macdonald.add_animal('sheep')
-macdonald.add_animal('sheep')
-macdonald.add_animal('goat', 12)
-macdonald.get_info()
+# Now, use the provided the_stranger.txt file and try using the class you created above.
 
-macdonald.get_short_info()
+
+class Text:
+    def __init__(self, text: str) -> None:
+        self.words: list[str] = text.split()
+
+    @classmethod
+    def from_file(cls, filename: str) -> "Text":
+        with open(filename) as f:
+            return Text(f.read())
+
+    def CalculateFrequency(self, word: str) -> float:
+        return self.words.count(word) / len(self.words)
+
+    def GetMostCommonWord(self) -> str:
+        # Set, so we do not recalculate count (O(n)) for each repetition
+        return max(set(self.words), key=self.words.count)
+
+    def GetUniqueWords(self) -> list[str]:
+        return list(set(self.words))
+
+    # This method will help to test our functionality
+    def DisplaySomeInfo(self, word="is") -> None:
+        print(f"Frequency of '{word}':", self.CalculateFrequency(word))
+        print("Most frequent word:", self.GetMostCommonWord())
+        print("Some of the unique words:", self.GetUniqueWords()[:10])
+        print("\n" + "=" * 30 + "\n")
+
+
+if __name__ == "__main__":
+    text = Text("Today, is a happy day. Truly a wonderful sight!")
+    text.DisplaySomeInfo()
+
+    filename = str(pathlib.Path(__file__).parent / "the_stranger.txt")
+    text = Text.from_file(filename)
+    text.DisplaySomeInfo()
