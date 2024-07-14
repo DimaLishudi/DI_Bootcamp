d92a80b9867cd541c7b8977863d99ad08ded139a
Dmitry Lishudi <lishudi.dmitry@gmail.com>
2024-07-14 18:04:50 +0300

    solve w7/d1 exercisexp

---
+++ Week7/ExerciseXP/script.js
@@ -0,0 +1,63 @@
+{
+    console.log("EXERCISE1")
+    // 🌟 Exercise 1 : Comparison
+    // Instructions
+    // Create a function called compareToTen(num) that takes a number as an argument.
+    // The function should return a Promise:
+    // the promise resolves if the argument is less than or equal to 10
+    // the promise rejects if argument is greater than 10.
+    // Test:
+
+    // //In the example, the promise should reject
+    // compareToTen(15)
+    //   .then(result => console.log(result))
+    //   .catch(error => console.log(error))
+
+    // //In the example, the promise should resolve
+    // compareToTen(8)
+    //   .then(result => console.log(result))
+    //   .catch(error => console.log(error))
+
+
+    function compareToTen(num) {
+        return new Promise((res, rej) => {
+            if (num <= 10) {
+                res(`${num} is ok`)
+            } else {
+                rej(`${num} is greater then 10`)
+            }
+        })
+    }
+
+    compareToTen(15)
+        .then(result => console.log(result))
+        .catch(error => console.log(error))
+
+    compareToTen(8)
+        .then(result => console.log(result))
+        .catch(error => console.log(error))
+}
+
+{
+    console.log("EXERCISE 2")
+    // 🌟 Exercise 2 : Promises
+    // Instructions
+    // Create a promise that resolves itself in 4 seconds and returns a “success” string.
+
+    setTimeout(
+        () => new Promise((res) => res("success")),
+         4 * 1000)
+}
+
+{
+    console.log("EXERCISE 3")
+    // 🌟 Exercise 3 : Resolve & Reject
+    // Instructions
+    // Use Promise.resolve(value) to create a promise that will resolve itself with a value of 3.
+    // Use Promise.reject(error) to create a promise that will reject itself with the string “Boo!”
+
+    Promise.resolve(3)
+        .then((res) => console.log(res))
+    Promise.reject("Boo!")
+        .catch((err) => console.log(err))
+}
